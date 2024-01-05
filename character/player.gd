78f5f988f7e3701f6484e3db76306b26d6978908
masterman555 <mr.master.ah82@gmail.com>
2024-01-05 18:57:38 +0330

    player movement v1.0.0

--- character/player.gd
+++ character/player.gd
@@ -2,15 +2,38 @@ extends CharacterBody2D
 
 signal health_depleted
 
+var max_speed = 500
+var accel = 5000
+var friction = 1000
 var health = 100.0
+var input = Vector2.ZERO
+
+func get_input():	
+	input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	return input.normalized()
+
 
 func _physics_process(delta):
 		# left = negative x, right = positive x, up = negative y, down = positive y
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down") 
-	velocity = direction * 500
-	move_and_slide()
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		#movement
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += (Input.get_vector("move_left", "move_right", "move_up", "move_down") * accel * delta)
+		velocity = velocity.limit_length(max_speed)
+	
+	if velocity.length() > 0:
+		#rotation
+		rotation = velocity.angle() + PI/2
+	
 	
 	if velocity.length() > 0.0:
+		#idle/walking animation
 		get_node("HappyBoo").play_walk_animation() # $HappyBoo.play_walk_animation - its the same
 	else:
 		get_node("HappyBoo").play_idle_animation()
@@ -22,3 +45,4 @@ func _physics_process(delta):
 		%ProgressBar.value = health
 		if health < 0.0:
 			health_depleted.emit()
+	move_and_slide()
